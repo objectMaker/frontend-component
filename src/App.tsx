f7baa1ac1fcafd48af26501066e0c01144ee4e4b
于邦飞 <528154320@qq.com>
2024-07-03 17:48:50 +0800

    create a card

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FirstCard from 'components/FirstCard'
+
+function App() {
+  return (
+    <>
+      <div className="p-12">
+        <FirstCard />
+      </div>
+    </>
+  )
+}
+
+export default App
